ea193a61f21f3ae6b84c992a4bf1a5116ed4abec
Martin Løland <martinloland@gmail.com>
2020-05-24 19:22:10 +0200

    updated sensor

--- dht.py
+++ dht.py
@@ -3,6 +3,9 @@ import Adafruit_DHT
 
 while True:
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(
+        sensor=Adafruit_DHT.AM2302,
+        pin=4
+    )
 
     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
